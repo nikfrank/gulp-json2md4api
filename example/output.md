990480e0a8aa6ff488c7a961cd37a88a34a08c72
nik frank <nikbfrank@gmail.com>
2015-11-16 04:24:38 +0200

    untested first version

--- example/output.md
+++ example/output.md
@@ -1,9 +1,81 @@
-# cani-dynamo
+# Cani-dynamo
+
+Cani-dynamo makes using AWS dynamoDB feel like actual noSQL
+
+```js
+Cani.dynamo.save('item', nuItem).then(...);
+Cani.dynamo.load('item', {owner:this.userId}).then(...);
+```
+
+
 
 ```html
 <script src="aws-sdk.js"></script>
 <script src="canijs/cani.js"></script>
 <script src="canijs/cani-dynamo/cani-dynamo.js"></script>
+<script src="Caniconfig.js"></script>
 ```
 
+just put this in your Caniconfig
 
+```js
+{
+   schemas:{
+      item:{
+         fields:{somehash:'S', id:'N', owner:'S', price:'N'},
+         table:{
+            arn:'arn:aws:dynamodb:eu-west-1:000000000000:table/tablName',
+            hashKey:'id', rangeKey:'somehash',
+            indices:(([])) // still needs to be implemented
+         }
+      }
+   },
+   awsConfigPack:{region: 'eu-west-1'},
+   initOn:['cognito: fb-login']
+}
+```
+
+see [`demo-caniconfig.js`](https://github.com/nikfrank/canijs/blob/master/src/docs/democonfig.js)
+for examples of all the config options for all the modules
+
+
+## Getting Started
+---
+
+```
+npm i -S canijs
+```
+
+then include the aws sdk, canijs core, cani-dynamo, and your Caniconfig.js of course
+
+
+## Basic use
+---
+
+```js
+Cani.dynamo.save('item', {
+   owner:this.userId, id:1440, somehash:'blahblahblah', price:2000
+}).then(function(res){
+   // I can't remember what res is here. Should probably check that!
+});
+```
+```js
+Cani.dynamo.load('item', {price:{LT:3000}).then(function(items){
+   console.log('there are '+items.length+' items under 3000 shmekels');
+});
+```
+
+Read the AWS [docs](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html)
+to learn about indices (GSI) that will let you query your data how you please
+
+
+## Examples
+---
+
+Available at ((link))
+
+
+## Full API
+---
+
+...
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var gutil = require('gulp-util');
 var through = require('through2');
+var pretty = require('json-prettify');
 
 module.exports = function (options) {
     return through.obj(function (file, enc, cb) {
@@ -28,8 +29,76 @@ module.exports = function (options) {
     });
 };
 
-module.exports.marked = marked;
-
 function json2md(src, opt, cb){
     // here render the md.
+
+    var template = 
+
+"# "+src.title+"\n\n"+src.missionStatement+
+"```js\n"+
+src.firstExample+
+"\n```\n\n\n"+
+"```html"+
+src.scripts.map((s) => ('<script src=\"'+s+'\"></script>')).join('\n')+
+"```\n\n"+
+"just put this in your Caniconfig\n\n"+
+"```js\n"+syntaxHighlight(src.configExample)+
+"\n```\n\n"+
+
+"see [`demo-caniconfig.js`](https://github.com/nikfrank/canijs/blob/master/src/docs/democonfig.js)\n"+
+"for examples of all the config options for all the modules\n\n\n"+
+
+"## Getting Started\n"+
+"---\n\n"+
+"```\n"+
+"npm i -S canijs\n"+
+"```\n\n"+
+
+"then include the aws sdk, canijs core, cani-dynamo, and your Caniconfig.js of course\n\n\n"+
+
+
+"## Basic use\n"+
+"---\n\n"+
+
+"```js\n"+
+src.basicUse.join('\n```\n```js\n')+
+"```\n\n"+
+
+"Read the AWS [docs](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html)\n"+
+"to learn about indices (GSI) that will let you query your data how you please\n\n\n"+
+
+
+"## Examples\n"+
+"---\n\n"+
+
+"Available at ((link))\n\n\n"+
+
+
+"## Full API\n"+
+"---\n\n"+
+
+"...'";
+
+    return template;
+
+}
+
+function syntaxHighlight(json){
+    json = JSON.stringify(json, null, 4);
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
 }
